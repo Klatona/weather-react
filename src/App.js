2c4eaf39b51a27ec8f4af4435abea653132bd8dc
Klatona <78326069+Klatona@users.noreply.github.com>
2021-05-01 13:31:17 -0400

    Added Overview.js & Overview.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import Form from "./Form";
+import Overview from "./Overview";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <div class="card body">
         <div class="card-body background">
           <Form />
-         
+         <Overview />
         </div>
       </div>
      
---
+++ src/Overview.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Overview.css";
+
+export default function Overview() {
+  let overviewData = {
+    city: "Toronto",
+    country: "CA",
+    date: "Saturday 10:00",
+    description: "cloudy"
+  };
+  return (
+    <div className="Overview">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="card-body">
+            <h1>
+              <span> {overviewData.city} </span>,
+              <span> {overviewData.country} </span>
+            </h1>
+            <h2>
+              <span> Last updated: {overviewData.date}</span>
+
+              <br />
+            </h2>
+            <h3>{overviewData.description}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
